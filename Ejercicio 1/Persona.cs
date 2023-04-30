4586a57ec7ebb9af818945b84fedebb2375e448d
fmartdom <fmartdom@outlook.com>
2023-04-30 15:03:46 -0300

    Agregar archivos de proyecto.

---
+++ Ejercicio 1/Persona.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_1
+{
+        class Persona {
+            //Propiedades
+            private string nombre;
+            private int sueldo;
+            private string email; 
+            private string sexo;
+            private int peso;
+            private decimal altura;
+
+            public Persona(string nombre, string email, int sueldo, string sexo, int peso,
+                decimal altura) { 
+                this.nombre = nombre;
+                this.sueldo = sueldo;
+                this.email = email;
+                this.sexo = sexo;
+                this.peso = peso;
+                this.altura = altura;
+                CrearEmail(nombre);
+
+            }
+
+            public void CrearEmail ( string nombre)
+        {
+             this.email = nombre.ToLower() + "@superempresa.com ";
+        }
+
+            public string GetEmail ()
+        {
+            return this.email;
+        }
+            public string GetNombre ()
+            {
+                return this.nombre;
+            }
+
+            public string GetSexo ()
+            {
+                return this.sexo;
+            }
+        //Devuelvo String porque es el tipo de dato que debe recibir la consola.
+            public string GetPeso ()
+            {
+                return this.peso.ToString();
+            }
+
+            public string GetSueldo ()
+            {
+                return this.sueldo.ToString();
+            }
+
+            public string GetAltura ()
+            {
+                return this.altura.ToString();
+            }
+
+        }
+    }
+
---
+++ Ejercicio 1/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Ejercicio_1
+{
+    internal class Program
+    {   
+                static void Main(string[] args)
+        {
+            string nombre;
+            string email;
+            string sexo;
+            int sueldo;
+            int peso;
+            decimal altura;
+            
+            email = string.Empty;
+
+            Console.WriteLine("Ingrese su nombre:");
+            nombre = Console.ReadLine();
+
+            Console.WriteLine("Ingrese su sexo");
+            sexo = Console.ReadLine();
+
+            Console.WriteLine("¿Cuál es su sueldo?");
+            sueldo = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("¿Cuál es su peso?");
+            peso = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("¿Cuál es su altura?");
+            altura = decimal.Parse(Console.ReadLine());
+
+            Console.WriteLine("Su email es: ");
+            
+            Persona nuevaPersona = new Persona (nombre, email, sueldo, sexo, peso, altura);
+
+            Console.WriteLine( 
+                    "Su nombre es: " + nuevaPersona.GetNombre() 
+                    + "Su sueldo es: " + nuevaPersona.GetSueldo()
+                    + "Su sexo es: " + nuevaPersona.GetSexo()
+                    + "Su peso es: " +  nuevaPersona.GetPeso() 
+                    + "Su altura es: " +   nuevaPersona.GetAltura()
+                    + "Su email es: " + nuevaPersona.GetEmail()
+                            );
+
+
+            Console.ReadLine();
+        }
+    }
+}
